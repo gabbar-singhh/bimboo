27fd6202bb2ec0d528e3f7762d9a7dc978b31a1d
gabbar-singhh <himanshup1308@gmail.com>
2024-07-22 14:35:06 +0530

    favicon updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,16 +7,19 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Bimboo",
-  description: "",
+  description: "we help software product companies to build ",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="logo.png" />
+      </head>
       <body className={inter.className}>
         {children}
         <Analytics />
-        <SpeedInsights/>
+        <SpeedInsights />
       </body>
     </html>
   );
